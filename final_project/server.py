70909d17f3217560a15b1fd6e8edc75aae8f0ead
cringel66 <127979127+cringel66@users.noreply.github.com>
2023-04-27 09:45:58 -0600

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -1,25 +1,27 @@
-from machinetranslation import translator
+""" server.py for translation program """
 from flask import Flask, render_template, request
+from machinetranslation import translator
 import json
 
 app = Flask("Language Translator")
 
 @app.route("/englishToFrench")
 def englishToFrench():
+    """ translate english to french """
     textToTranslate = request.args.get('textToTranslate')
     frenchtext=translator.englishToFrench(textToTranslate)
-    return "Translated to French" %frenchtext
+    return frenchtext
 
 @app.route("/frenchToEnglish")
-def englishToFrench():
+def frenchToEnglish():
+    """ translate french to english """
     textToTranslate = request.args.get('textToTranslate')
     englishtext=translator.frenchToEnglish(textToTranslate)
-    return "Translated to English" %englishtext
+    return englishtext
 
 @app.route("/")
 def homepage():
     return render_template("index.html")
 
 if __name__=="__main__":
-    app.run(debug=True) 
-    # When no port is specified, starts at default port 5000
+    app.run(debug=True)  # When no port is specified, starts at default port 5000
